17661507a78f88ca0764d232ee4408ade3a140f1
h2n0 <elc954@hotmail.co.uk>
2016-01-05 11:28:18 +0000

    not a lot

--- links/code.js
+++ links/code.js
@@ -14,7 +14,7 @@ $("tiny").onclick = function(){
 }
 
 $("prev").onclick = function(){
-	setLink(prev[prev.length - 1 - currentPos]);
+	setLink(prev[prev.length - 2 - currentPos]);
 	currentPos ++;
 	if(currentPos >= prev.length - 1)currentPos = prev.length - 1;
 }
