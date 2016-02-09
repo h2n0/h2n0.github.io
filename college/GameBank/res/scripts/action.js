b2140f17249ceb286846052923f43429c43b7818
h2n0 <elc954@hotmail.co.uk>
2016-02-09 09:31:21 +0000

    College things

---
+++ college/GameBank/res/scripts/action.js
@@ -0,0 +1,64 @@
+var bubble = null;
+var cartDetail = null;
+var priceTag = null;
+
+var price = 0;
+var inBasket = 0;
+
+function removeClass(dom,s){
+  var res = ""
+  parts = dom.className.split(" ");
+  for(var i = 0; i < parts.length; i++){
+    if(parts[i] != s){
+      res += parts[i]+" ";
+    }
+  }
+  dom.className = res;
+}
+
+function addClass(dom, s){
+  dom.className += " " + s;
+}
+
+function addToBasket(p){
+  price += p;
+  inBasket ++;
+  updateBubble();
+  priceTag.innerHTML = "£"+price;
+}
+
+
+function updateBubble(){
+  bubble.innerHTML = inBasket;
+  removeClass(bubble,"hidden");
+}
+
+function cartOver(){
+  if(inBasket > 0)
+  removeClass(cartDetail,"hidden");
+}
+
+function cartLeave(){
+  if(inBasket > 0)
+  addClass(cartDetail,"hidden");
+}
+
+function getPrice(dom, price){
+  var newPrice = "" + (price / 44.444);
+  newPrice = newPrice.substring(0,6);
+  dom.innerHTML = newPrice;
+}
+
+
+
+window.onload = function(){
+  var $ = function(id){
+    return document.getElementById(id);
+  }
+
+  bubble = $("basketCount");
+  cartDetail = $("basketDet");
+  priceTag = $("cost");
+  $("cart").addEventListener("mouseover",cartOver);
+  $("cart").addEventListener("mouseout", cartLeave);
+}
